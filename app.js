dcc3f9136ac8070ad8583fe6b02fbd4021fb8378
kianu <thomasulee@gmail.com>
2023-03-14 11:41:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -26,12 +26,8 @@ app.use(
 
 //Cors Configuration - Start
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET", true);
-        return res.status(200).end();
-  }
   next();
 })
 //Cors Configuration - End
