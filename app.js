6a82f25435e4ba24e369470eb6c45fde389d3fde
kianu <thomasulee@gmail.com>
2023-03-14 11:38:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === "OPTIONS") {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET", true);
-        return res.status(200).json({});
+        return res.status(200).end();
   }
   next();
 })
